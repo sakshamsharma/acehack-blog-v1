99349cb167e533ea29d42bf41980e04a5978d10b
Saksham Sharma <saksham0808@gmail.com>
2015-07-14 11:41:11 +0530

    Now limited items in sidebar

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.get('/', function (req, res)
   fs.readFile("public/md/reveries.md", 'utf8', function(err, data) {
     if (err) res.render('articles', { title: 'Articles', md:md, markdownContent:"Sorry, the given file does not exist."})
     else
-      connection.query('SELECT * from Technical', function(err, newtech, fields){
-        connection.query('SELECT * from Musings', function(err, newmus, fields){
+      connection.query('SELECT * from Technical ORDER BY Id DESC LIMIT 6', function(err, newtech, fields){
+        connection.query('SELECT * from Musings ORDER BY Id DESC LIMIT 6', function(err, newmus, fields){
           res.render('index', { title: 'AceHack', md:md, markdownContent:data, technical: newtech, musings: newmus})
         })
       })
@@ -67,8 +67,8 @@ app.get('/technical', function(req, res) {
     if (err || rows.length == 0) res.render('error', { title: 'Error', md:md, markdownContent:"Sorry, the given file does not exist."})
     else {
       var data = new Buffer(rows[0].Content, 'base64').toString()
-      connection.query('SELECT * from Technical', function(err, newtech, techfields){
-        connection.query('SELECT * from Musings', function(err, newmus, fields){
+      connection.query('SELECT * from Technical ORDER BY Id DESC LIMIT 6', function(err, newtech, techfields){
+        connection.query('SELECT * from Musings ORDER BY Id DESC LIMIT 6', function(err, newmus, fields){
           res.render('viewer', { title: rows[0].Name, md:md, markdownContent:data, technical: newtech, musings: newmus})
         })
       })
@@ -81,8 +81,8 @@ app.get('/musings', function(req, res) {
     if (err || rows.length == 0) res.render('error', { title: 'Error', md:md, markdownContent:"Sorry, the given file does not exist."})
     else {
       var data = new Buffer(rows[0].Content, 'base64').toString()
-      connection.query('SELECT * from Technical', function(err, newtech, techfields){
-        connection.query('SELECT * from Musings', function(err, newmus, fields){
+      connection.query('SELECT * from Technical ORDER BY Id DESC LIMIT 6', function(err, newtech, techfields){
+        connection.query('SELECT * from Musings ORDER BY Id DESC LIMIT 6', function(err, newmus, fields){
           res.render('viewer', { title: rows[0].Name, md:md, markdownContent:data, technical: newtech, musings: newmus})
         })
       })
@@ -94,8 +94,8 @@ app.get('/almanac', function(req, res) {
   fs.readFile("public/md/almanac.md", 'utf8', function(err, data) {
     if (err) res.render('viewer', { title: 'Error', md:md, markdownContent:"Sorry, the given file does not exist."})
     else
-      connection.query('SELECT * from Technical', function(err, newtech, techfields){
-        connection.query('SELECT * from Musings', function(err, newmus, fields){
+      connection.query('SELECT * from Technical ORDER BY Id DESC LIMIT 6', function(err, newtech, techfields){
+        connection.query('SELECT * from Musings ORDER BY Id DESC LIMIT 6', function(err, newmus, fields){
           res.render('viewer', { title: 'Almanac', md:md, markdownContent:data, technical: newtech, musings: newmus})
         })
       })
@@ -103,11 +103,11 @@ app.get('/almanac', function(req, res) {
 })
 
 app.get('/test', function(req, res) {
-  fs.readFile("public/md/test.md", 'utf8', function(err, data) {
+  fs.readFile("public/md/almanac.md", 'utf8', function(err, data) {
     if (err) res.render('viewer', { title: 'Error', md:md, markdownContent:"Sorry, the given file does not exist."})
     else
-      connection.query('SELECT * from Technical', function(err, newtech, techfields){
-        connection.query('SELECT * from Musings', function(err, newmus, fields){
+      connection.query('SELECT * from Technical ORDER BY Id DESC LIMIT 6', function(err, newtech, techfields){
+        connection.query('SELECT * from Musings ORDER BY Id DESC LIMIT 6', function(err, newmus, fields){
           res.render('viewer', { title: 'Almanac', md:md, markdownContent:data, technical: newtech, musings: newmus})
         })
       })
